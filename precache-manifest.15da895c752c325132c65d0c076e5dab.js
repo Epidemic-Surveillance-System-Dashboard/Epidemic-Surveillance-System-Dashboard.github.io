a4a3679bf2821e96fdbde80c5be578cec8c39ce4
Laban <labanlin@me.com>
2019-03-15 19:11:43 -0400

    build

---
+++ precache-manifest.15da895c752c325132c65d0c076e5dab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6473145699ff0abf9dac",
+    "url": "/static/css/main.5b5bf4ee.chunk.css"
+  },
+  {
+    "revision": "6473145699ff0abf9dac",
+    "url": "/static/js/main.64731456.chunk.js"
+  },
+  {
+    "revision": "643d830645e60a444294",
+    "url": "/static/css/1.b3f5463b.chunk.css"
+  },
+  {
+    "revision": "643d830645e60a444294",
+    "url": "/static/js/1.643d8306.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4822ff90b852cc741129a4604f0502b0",
+    "url": "/index.html"
+  }
+];
